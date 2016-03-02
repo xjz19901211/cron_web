c370308af6afd243ab60ea7f4b897d58224f450a
jiangzhi.xie <xiejiangzhi@gmail.com>
2016-03-02 19:47:04 +0800

    Fix production xx_url, Add username to user_logs

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module CronWeb
   class Application < Rails::Application
+
+    require Rails.root.join('lib/settings')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,6 +25,8 @@ module CronWeb
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    Rails.application.routes.default_url_options[:host] = URI.parse(Settings.base['host_url']).host
 
     config.generators do |g|
       g.helper false
